be4e45d0868eb61272759610269858141c4e8e2d
Luke Madronal <madronal@lafayette.edu>
2015-11-13 11:06:16 -0500

    sign in working correctly.

--- AttendanceLog/AttendanceLog/AppDelegate.swift
+++ AttendanceLog/AttendanceLog/AppDelegate.swift
@@ -9,17 +9,120 @@
 import UIKit
 import Parse
 import Bolts
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, ESTBeaconManagerDelegate {
+    
     var window: UIWindow?
-
-
+    let beaconManager = ESTBeaconManager()
+    var lastRegion: CLBeaconRegion?
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         Parse.setApplicationId("xBguYb7K8F3qt2gEmhzkmLrUnPUv4Nqo3gjUmmd6",
             clientKey: "4FfN0YC7BzxJohPsmS2bNWpuw6IXVq4sDs1WZQoA")
+        self.beaconManager.delegate = self
+        checkForLocationAuthorization()
         return true
     }
 }
+
+extension AppDelegate {
+    func beaconManager(manager: AnyObject, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            let nearestBeacon = beacons[0]
+            //if region != lastRegion {
+            switch nearestBeacon.proximity {
+            case .Immediate:
+                dispatch_async(dispatch_get_main_queue()) {
+                    if (region.identifier == "MintBeacon") {
+                        NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "recievedDataFromBlue", object: nil))
+                    } else if (region.identifier == "BlueBeacon") {
+                        NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "recievedDataFromMint", object: nil))
+                    }
+                    NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "recievedDataFromServer", object: nil))
+                }
+                print("Ranged Immediate \(region.identifier) beacon")
+            case .Near:
+                print("Ranged near \(region.identifier) beacon")
+            case .Far:
+                print("Ranged far \(region.identifier) beacon")
+            case .Unknown:
+                print("Ranged unknown \(region.identifier) beacon")
+            }
+            lastRegion = region
+            //}
+        }
+    }
+    
+    func beaconManager(manager: AnyObject, didDetermineState state: CLRegionState, forRegion region: CLBeaconRegion) {
+        switch state {
+        case .Unknown:
+            print("region \(region.identifier) unknown")
+        case .Inside:
+            print("inside \(region.identifier) region")
+        case.Outside:
+            print("outside \(region.identifier) region")
+        }
+    }
+    
+    func beaconManager(manager: AnyObject, didEnterRegion region: CLBeaconRegion) {
+        print("did enter region \(region.identifier)")
+    }
+    
+    func beaconManager(manager: AnyObject, didExitRegion region: CLBeaconRegion) {
+        print("Did exit \(region.identifier)")
+    }
+    
+    func setUpBeacons() {
+        print("setting up beacons")
+        let uuidString = "B9407F30-F5F8-466E-AFF9-25556B57FE6D"
+        let beaconUUID = NSUUID(UUIDString: uuidString)!
+        
+        let beaconIdentifier = "Iron Yard"
+        let allBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID, identifier: beaconIdentifier)
+        beaconManager.startMonitoringForRegion(allBeaconRegion)
+        
+        //        let beacon1Major: CLBeaconMajorValue = 39380
+        //        let beacon1Minor: CLBeaconMinorValue = 44024
+        //        let beacon1Identifier = "PurpleBeacon"
+        //        let purpleBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID, major: beacon1Major, minor: beacon1Minor, identifier: beacon1Identifier)
+        //        beaconManager.startRangingBeaconsInRegion(purpleBeaconRegion)
+        
+        let beacon2Major: CLBeaconMajorValue = 31640
+        let beacon2Minor: CLBeaconMinorValue = 65404
+        let beacon2Identifier = "BlueBeacon"
+        let blueBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID, major: beacon2Major, minor: beacon2Minor, identifier: beacon2Identifier)
+        beaconManager.startRangingBeaconsInRegion(blueBeaconRegion)
+        
+        let beacon3Major: CLBeaconMajorValue = 34909
+        let beacon3Minor: CLBeaconMinorValue = 15660
+        let beacon3Identifier = "MintBeacon"
+        let mintBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID, major: beacon3Major, minor: beacon3Minor, identifier: beacon3Identifier)
+        beaconManager.startRangingBeaconsInRegion(mintBeaconRegion)
+    }
+    
+    func checkForLocationAuthorization() {
+        if CLLocationManager.locationServicesEnabled() {
+            print("location services on")
+            switch ESTBeaconManager.authorizationStatus() {
+            case .AuthorizedAlways, .AuthorizedWhenInUse:
+                print("start up")
+                setUpBeacons()
+            case .Denied,.Restricted:
+                print("hey user, turn us on in settings")
+            case .NotDetermined:
+                print("not determined")
+                if beaconManager.respondsToSelector("requestAlwaysAuthorization") {
+                    print("Requesting Always")
+                    beaconManager.requestAlwaysAuthorization()
+                }
+            }
+        } else {
+            print("turn on location services!!")
+        }
+    }
+}
+
 
--- AttendanceLog/AttendanceLog/ViewController.swift
+++ AttendanceLog/AttendanceLog/ViewController.swift
@@ -7,19 +7,202 @@
 //
 
 import UIKit
+import ParseUI
+import Parse
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate  {
+    //@IBOutlet var loginButton :UIBarButtonItem!
+    
+    //MARK: - User Default Methods
+    func setUsernameDefault(username: String) {
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setObject(username, forKey: "DefaultUsername")
+        userDefaults.synchronize()
+    }
+    
+    func getUserNameDefault() ->String {
+        if let defaultUserName = NSUserDefaults.standardUserDefaults().stringForKey("DefaultUsername") {
+            return defaultUserName
+        } else {
+            return ""
+        }
+    }
+    
+    
+    //MARK: - Interactivity Methods
+    //    @IBAction func addRecord(sender: UIBarButtonItem) {
+    //        if let uCurrentUser = PFUser.currentUser() {
+    //            let checkedIn = PFObject(className: "ToDo")
+    //            checkedIn.ACL = PFACL(user: uCurrentUser)
+    //            checkedIn["toDoDesc"] = toDoTextField.text
+    //            checkedIn.saveInBackgroundWithBlock({ (success,error) -> Void in
+    //                if success {
+    //                    let alert = UIAlertController(title: "Saved", message: "Your to do was saved!", preferredStyle: .Alert)
+    //                    alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+    //                    self.presentViewController(alert, animated: true, completion: nil)
+    //                } else {
+    //                    let alert = UIAlertController(title: "NOT Saved", message: "Your to do was NOT saved!", preferredStyle: .Alert)
+    //                    alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+    //                    self.presentViewController(alert, animated: true, completion: nil)
+    //                }
+    //            })
+    //        } else {
+    //            let alert = UIAlertController(title: "No User", message: "You are not logged in! Go log in first!", preferredStyle: .Alert)
+    //            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+    //            self.presentViewController(alert, animated: true, completion: nil)
+    //        }
+    //    }
+    
+    //MARK: - Login Methods
+    
+    //    @IBAction func loginButtonPressed(sender: UIBarButtonItem) {
+    //        if let _ = PFUser.currentUser() {
+    //            PFUser.logOut()
+    //            loginButton.title = "Log In"
+    //        } else {
+    //            //this needs to be fixed
+    //
+    //            let loginController = PFLogInViewController()
+    //            loginController.delegate = self
+    //            let signupController = PFSignUpViewController()
+    //            signupController.delegate = self
+    //            loginController.signUpController = signupController
+    //            loginController.logInView?.usernameField?.text = getUserNameDefault()
+    //            presentViewController(loginController, animated: true, completion: nil)
+    //
+    //        }
+    //    }
+    
+    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
+        dismissViewControllerAnimated(true, completion: nil)
+        setUsernameDefault(logInController.logInView!.usernameField!.text!)
+        //loginButton.title = "Log Out"
+    }
+    
+    func logInViewControllerDidCancelLogIn(logInController: PFLogInViewController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func signUpViewControllerDidCancelSignUp(signUpController: PFSignUpViewController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    //MARK: - Parse Date Recieved Methods
+    
+    func newDataRecieved(currentUser: PFUser, inOut: String) {
+        let checkedIn = PFObject(className: "checkedIn")
+        checkedIn.ACL = PFACL(user: currentUser)
+        checkedIn["userStatus"] = currentUser.username!+inOut
+        checkedIn.saveInBackgroundWithBlock({ (success,error) -> Void in
+            if success {
+                let alert = UIAlertController(title: "Checked In!", message: "You're in! Have a great day", preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+        })
+    }
+    
+    func fetchCurrentlySignedInUser() {
+        if let uCurrentUser = PFUser.currentUser() {
+            let query = PFQuery(className:"checkedIn")
+            query.whereKey("userStatus", equalTo:uCurrentUser.username!+"In")
+            query.findObjectsInBackgroundWithBlock {
+                (objects: [PFObject]?, error: NSError?) -> Void in
+                
+                if error == nil {
+                    // The find succeeded.
+                    print("Successfully retrieved \(objects!.count) scores.")
+                    
+                    if (objects!.count == 0) {
+                        self.newDataRecieved(uCurrentUser, inOut: "In")
+                    } else if let uObjects = objects {
+                        print("userIn is \(String(uObjects[0]["userIn"]))")
+                        if !(String(uObjects[0]["userStatus"]) == uCurrentUser.username!+"In") {
+                            print("there is stuff in the array")
+                            self.newDataRecieved(uCurrentUser,inOut: "In")
+                        }
+                    } else {
+                        print("got into nothing in the array")
+                    }
+                } else {
+                    // Log details of the failure
+                    //print("Error: \(error!) \(error!.userInfo!)")
+                }
+            }
+        } else {
+            let alert = UIAlertController(title: "No User", message: "You are not logged in! Go log in first!", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func fetchCurrentlySignedOutUser() {
+        if let uCurrentUser = PFUser.currentUser() {
+            let query = PFQuery(className:"checkedIn")
+            query.whereKey("userStatus", equalTo:uCurrentUser.username!+"Out")
+            query.findObjectsInBackgroundWithBlock {
+                (objects: [PFObject]?, error: NSError?) -> Void in
+                
+                if error == nil {
+                    // The find succeeded.
+                    print("Successfully retrieved \(objects!.count) scores.")
+                    
+                    if (objects!.count == 0) {
+                        self.newDataRecieved(uCurrentUser,inOut: "Out")
+                    } else if let uObjects = objects {
+                        print("userIn is \(String(uObjects[0]["userIn"]))")
+                        if !(String(uObjects[0]["userIn"]) == uCurrentUser.username!+"Out") {
+                            print("there is stuff in the array")
+                            self.newDataRecieved(uCurrentUser,inOut: "Out")
+                        }
+                    } else {
+                        print("got into nothing in the array")
+                    }
+                } else {
+                    // Log details of the failure
+                    //print("Error: \(error!) \(error!.userInfo!)")
+                }
+            }
+        } else {
+            let alert = UIAlertController(title: "No User", message: "You are not logged in! Go log in first!", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    //MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        PFUser.logOut()
+        if let _ = PFUser.currentUser() {
+            PFUser.logOut()
+            //loginButton.title = "Log In"
+        } else {
+            //this needs to be fixed
+            
+            let loginController = PFLogInViewController()
+            loginController.delegate = self
+            let signupController = PFSignUpViewController()
+            signupController.delegate = self
+            loginController.signUpController = signupController
+            loginController.logInView?.usernameField?.text = getUserNameDefault()
+            presentViewController(loginController, animated: true, completion: nil)
+            
+        }
+        //loginButton.title = "LogIn"
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "fetchCurrentlySignedOutUser", name: "recievedDataFromMint", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "fetchCurrentlySignedInUser", name: "recievedDataFromBlue", object: nil)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
